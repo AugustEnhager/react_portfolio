2ebb3c16c754fe6b037f3b3ad31d2675484ecf5d
August <august.enhager@gmail.com>
2021-09-07 08:23:58 +0200

    adds the first component and hook it into the DOM

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWolrd = () => {
+    return <h2>Hello Wolrd</h2>;
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
