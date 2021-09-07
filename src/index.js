d302b33bd47dfe35f1bb580cb3328171df90d3f9
August <august.enhager@gmail.com>
2021-09-07 09:35:32 +0200

    initiates project and adds gitignore scaffold

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const HelloWolrd = () => {
-    return <h2>Hello Wolrd</h2>;
-};
-
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
